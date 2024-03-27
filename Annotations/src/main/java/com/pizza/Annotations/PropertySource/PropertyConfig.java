4b54c164e53e72a7be26a18d229f946b29c000b1
Tasawar Abbas <taswarabbassss@gmail.com>
2024-03-27 22:14:03 +0500

    Use of @PropertySources Annotation to Configure Multiple Custom Property Files

--- Annotations/src/main/java/com/pizza/Annotations/PropertySource/PropertyConfig.java
+++ Annotations/src/main/java/com/pizza/Annotations/PropertySource/PropertyConfig.java
@@ -2,10 +2,16 @@ package com.pizza.Annotations.PropertySource;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @Configuration
-@PropertySource("classpath:mail.properties")
-@PropertySource("classpath:message.properties")
+// @PropertySource("classpath:mail.properties")
+// @PropertySource("classpath:message.properties")
+
+@PropertySources({
+    @PropertySource("classpath:mail.properties"),
+    @PropertySource("classpath:message.properties")
+})
 public class PropertyConfig {
     
 }
