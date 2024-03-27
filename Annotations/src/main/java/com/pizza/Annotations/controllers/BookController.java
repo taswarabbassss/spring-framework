c0689364b6e91781ecd2ba2e09686eee564a554e
Tasawar Abbas <taswarabbassss@gmail.com>
2024-03-27 23:27:24 +0500

    Accessing URL of the request withing request handling method

--- Annotations/src/main/java/com/pizza/Annotations/controllers/BookController.java
+++ Annotations/src/main/java/com/pizza/Annotations/controllers/BookController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.pizza.Annotations.controllers.entities.Book;
 
+import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class BookController {
     
     @RequestMapping(value = {"/book","/java","/core"}, method=RequestMethod.GET)
-    public Book getBook() {
+    public Book getBook(HttpServletRequest request) {
+        String url =  request.getRequestURL().toString();
+        System.out.println("URL: "+ url);
         return new Book(1, "Java-Core", "Tasawar");
     }
     
