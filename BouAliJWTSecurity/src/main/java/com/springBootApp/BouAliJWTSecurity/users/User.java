42c7d8d928f9a8f83c21c012ec1b02bcd568aedb
Tasawar Abbas <taswarabbassss@gmail.com>
2024-03-29 01:29:08 +0500

    User POJO class

---
+++ BouAliJWTSecurity/src/main/java/com/springBootApp/BouAliJWTSecurity/users/User.java
@@ -0,0 +1,20 @@
+package com.springBootApp.BouAliJWTSecurity.users;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
