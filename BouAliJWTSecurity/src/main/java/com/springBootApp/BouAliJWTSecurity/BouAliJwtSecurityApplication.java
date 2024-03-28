99d29545f98bd41acaa2a4fb6efda8bbd721bcd2
Tasawar Abbas <taswarabbassss@gmail.com>
2024-03-29 00:48:47 +0500

    Initial Setup for Json Web Token implementation with Spring Security

---
+++ BouAliJWTSecurity/src/main/java/com/springBootApp/BouAliJWTSecurity/BouAliJwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.springBootApp.BouAliJWTSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BouAliJwtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BouAliJwtSecurityApplication.class, args);
+	}
+
+}
---
+++ BouAliJWTSecurity/src/test/java/com/springBootApp/BouAliJWTSecurity/BouAliJwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootApp.BouAliJWTSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BouAliJwtSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
