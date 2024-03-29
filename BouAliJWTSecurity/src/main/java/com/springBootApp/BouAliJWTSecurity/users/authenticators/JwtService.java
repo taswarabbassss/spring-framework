ec5a60f7dde1b4ee7e92682c7e91e0872e715881
Tasawar Abbas <taswarabbassss@gmail.com>
2024-03-30 00:10:17 +0500

    Extract ClaimsMethod Implementation

--- BouAliJWTSecurity/src/main/java/com/springBootApp/BouAliJWTSecurity/users/authenticators/JwtService.java
+++ BouAliJWTSecurity/src/main/java/com/springBootApp/BouAliJWTSecurity/users/authenticators/JwtService.java
@@ -1,10 +1,24 @@
 package com.springBootApp.BouAliJWTSecurity.users.authenticators;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class JwtService {
     public String extractUsername(String token) {
         return null;
     }
+
+    private Claims extractAllClaims(String token){
+
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
 }
