3ad221f371aee79cc794330b4f26d4edb25eb35d
Tasawar Abbas <taswarabbassss@gmail.com>
2024-04-01 22:13:40 +0500

    Added check at user authenticated or userEmail is null.

--- BouAliJWTSecurity/src/main/java/com/springBootApp/BouAliJWTSecurity/users/config/JwtAuthenticationFilter.java
+++ BouAliJWTSecurity/src/main/java/com/springBootApp/BouAliJWTSecurity/users/config/JwtAuthenticationFilter.java
@@ -1,4 +1,4 @@
-package com.springBootApp.BouAliJWTSecurity.users.authenticators;
+package com.springBootApp.BouAliJWTSecurity.users.config;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,6 +19,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private  final  JwtService jwtService;
+    private final UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -32,6 +36,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt); // todo extract userEmail from JWT Token;
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication()==null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+        }
 
     }
 }
