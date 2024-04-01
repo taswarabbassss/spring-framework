54cfa79426d8ce4bc00c6f2dd306f11d92bcd4a5
Tasawar Abbas <taswarabbassss@gmail.com>
2024-04-02 00:07:11 +0500

    Implemented Registered User method

--- BouAliJWTSecurity/src/main/java/com/springBootApp/BouAliJWTSecurity/users/services/Impl/AuthenticationServiceImpl.java
+++ BouAliJWTSecurity/src/main/java/com/springBootApp/BouAliJWTSecurity/users/services/Impl/AuthenticationServiceImpl.java
@@ -1,18 +1,38 @@
 package com.springBootApp.BouAliJWTSecurity.users.services.Impl;
 
+import com.springBootApp.BouAliJWTSecurity.users.Role;
+import com.springBootApp.BouAliJWTSecurity.users.User;
+import com.springBootApp.BouAliJWTSecurity.users.UserRepository;
+import com.springBootApp.BouAliJWTSecurity.users.config.JwtService;
 import com.springBootApp.BouAliJWTSecurity.users.entities.AuthenticationRequest;
 import com.springBootApp.BouAliJWTSecurity.users.entities.AuthenticationResponse;
 import com.springBootApp.BouAliJWTSecurity.users.entities.RegisterRequest;
 import com.springBootApp.BouAliJWTSecurity.users.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
     @Override
     public AuthenticationResponse registerUser(RegisterRequest request) {
-        return null;
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        String jwtToken = jwtService.generateToken(new HashMap<>(), user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+
     }
 
     @Override
