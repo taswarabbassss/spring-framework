8c31ea0c231869214bc9aa3bbd75c389b45e5e12
Tasawar Abbas <taswarabbassss@gmail.com>
2024-04-02 00:15:59 +0500

    Implemented AuthenticateUser method

--- BouAliJWTSecurity/src/main/java/com/springBootApp/BouAliJWTSecurity/users/services/Impl/AuthenticationServiceImpl.java
+++ BouAliJWTSecurity/src/main/java/com/springBootApp/BouAliJWTSecurity/users/services/Impl/AuthenticationServiceImpl.java
@@ -9,6 +9,8 @@ import com.springBootApp.BouAliJWTSecurity.users.entities.AuthenticationResponse
 import com.springBootApp.BouAliJWTSecurity.users.entities.RegisterRequest;
 import com.springBootApp.BouAliJWTSecurity.users.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,8 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
     @Override
     public AuthenticationResponse registerUser(RegisterRequest request) {
         var user = User.builder()
@@ -37,6 +41,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public AuthenticationResponse authenticateUser(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                request.getEmail(),
+                request.getPassword())
+        );
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        String jwtToken = jwtService.generateToken(new HashMap<>(), user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
     }
 }
