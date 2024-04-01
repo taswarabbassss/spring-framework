3839d129cdb7abecc285265de6dee1bd1094a79a
Tasawar Abbas <taswarabbassss@gmail.com>
2024-04-01 21:23:15 +0500

    Implemented other JWT necessary methods

--- BouAliJWTSecurity/src/main/java/com/springBootApp/BouAliJWTSecurity/users/authenticators/JwtService.java
+++ BouAliJWTSecurity/src/main/java/com/springBootApp/BouAliJWTSecurity/users/authenticators/JwtService.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
-import java.util.Base64;
 import java.util.Date;
 import java.util.Map;
 import java.util.function.Function;
@@ -32,6 +31,19 @@ public class JwtService {
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
                 .signWith(getSignInKey())
                 .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractSingleClaim(token,Claims::getExpiration);
     }
 
     public String extractUsername(String token) {
